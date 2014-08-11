95c374f662e20544b09e7e80e5e148591fc58882
Daniel Leining <daniel@the-beach.co>
2014-08-11 17:42:51 -0400

    add stuff

--- log/log.go
+++ log/log.go
@@ -2,10 +2,13 @@ package log
 
 import "fmt"
 
+// print to log with level INFO
 func I(msg string) {
 	fmt.Printf("INFO: %s\n", msg)
 }
 
+
+//print to log with level ERROR
 func E(msg string) {
 	fmt.Printf("ERROR: %s\n", msg)
 }
