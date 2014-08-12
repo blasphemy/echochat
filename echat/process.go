002c2eb2e1afd495c5f2eceaa834d2da4b9d729c
Daniel Leining <daniel@the-beach.co>
2014-08-11 21:14:01 -0400

    implement quit stashing work

--- echat/process.go
+++ echat/process.go
@@ -1,7 +1,15 @@
 //process lines
 package main
 
+import "strings"
+
 //takes a line and a user and processes it.
 func ProcessLine(user *User, msg string) {
 	user.SendLine(msg)
+	args := strings.Split(msg, " ")
+	switch args[0] {
+	case "QUIT":
+		user.SendLine("Lol QUIT!!!")
+		user.Quit()
+	}
 }
--- echat/user.go
+++ echat/user.go
@@ -12,17 +12,23 @@ type User struct {
 	user       string
 	ident      string
 	ip         string
+	dead       bool
 	connection net.Conn
+}
+
+func (user *User) Quit() {
+	user.dead = true
+	user.connection.Close()
 }
 
 //set a user's nick. Does not do any error checking
 func (user *User) SetNick(new string) {
-  user.nick = new
+	user.nick = new
 }
 
 //returns a user's nick in string format
 func (user *User) GetNick() string {
-  return user.nick
+	return user.nick
 }
 
 func (user *User) SetConn(in net.Conn) {
@@ -43,16 +49,17 @@ func (user *User) HandleRequests() {
 	conn := user.GetConn()
 	b := bufio.NewReader(conn)
 	for {
+		if user.dead {
+			break
+		}
 		line, err := b.ReadString('\n')
 		if err != nil {
 			fmt.Println("Error reading:", err.Error())
-			break
+			user.Quit()
 		}
 		line = strings.TrimSpace(line)
 		fmt.Println("Received Line: ", line)
 		// Send a response back to person contacting us.
 		go ProcessLine(user, line)
-
 	}
-	conn.Close()
 }
