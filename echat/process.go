62edddc94bbb874ba41ee6291b191b24f3d579a1
Daniel Leining <daniel@the-beach.co>
2014-08-12 20:59:19 -0400

    remove stupid debugging line

--- echat/process.go
+++ echat/process.go
@@ -5,7 +5,6 @@ import "strings"
 
 //takes a line and a user and processes it.
 func ProcessLine(user *User, msg string) {
-	user.SendLine(msg)
 	args := strings.Split(msg, " ")
 	checkme := strings.ToLower(args[0])
 	switch checkme {
