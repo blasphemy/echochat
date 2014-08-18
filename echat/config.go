03e6ae8b75d9b2b8f6d127de0547ba3653372c99
Daniel Leining <daniel@the-beach.co>
2014-08-18 03:11:24 -0400

    Add comment to config.go

--- echat/config.go
+++ echat/config.go
@@ -8,7 +8,7 @@ const (
 	CONN_PORT       = "6667"
 	ping_time       = 45 //something
 	ping_check_time = 20 // time between the user's ping checks
-	resolvehosts    = false
+	resolvehosts    = false //Note: make forward confirmed reverse dns optional.
 	isupport        = "NAMESX CHANTYPES=#& PREFIX=(o)@"
 )
 
