74586f93351c73430ef362381712465ee8f4f852
Daniel Leining <daniel@the-beach.co>
2014-08-18 12:16:09 -0400

    go fmt

--- echat/config.go
+++ echat/config.go
@@ -6,9 +6,9 @@ const (
 	softwarev       = "v0.1"
 	CONN_HOST       = "0.0.0.0"
 	CONN_PORT       = "6667"
-	ping_time       = 45 //something
-	ping_check_time = 20 // time between the user's ping checks
-	resolvehosts    = true  //Note: make forward confirmed reverse dns optional.
+	ping_time       = 45   //something
+	ping_check_time = 20   // time between the user's ping checks
+	resolvehosts    = true //Note: make forward confirmed reverse dns optional.
 	isupport        = "NAMESX CHANTYPES=#& PREFIX=(o)@"
 )
 
