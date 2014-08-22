d6f2c927071ed0183a247b4146ad66ab660d1424
Daniel Leining <daniel@the-beach.co>
2014-08-21 20:33:17 -0400

    read config file

--- echat/config.go
+++ echat/config.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
+	"os"
 	"time"
 )
 
@@ -21,6 +23,20 @@ type configuration struct {
 }
 
 func SetupConfig() {
+	confile, err := ioutil.ReadFile("echochat.json")
+	if err != nil {
+		log.Print("Error reading config file echochat.json: " + err.Error())
+		os.Exit(1)
+	} else {
+		err := json.Unmarshal(confile, &config)
+		if err != nil {
+			log.Print("Error parsing config file: " + err.Error())
+			os.Exit(1)
+		}
+	}
+}
+
+func SetupConfigDefault() {
 	config = configuration{
 		ServerName:        "test.net.local",
 		DefaultKickReason: "Your behavior is not conductive of the desired environment.",
@@ -34,11 +50,12 @@ func SetupConfig() {
 		ListenIps:         []string{"0.0.0.0"},
 		ListenPorts:       []int{6667, 6668},
 	}
-	k, err := json.Marshal(config)
+	k, err := json.MarshalIndent(config, "", "\t")
 	if err != nil {
 		log.Print(err.Error())
 	}
 	log.Print(string(k))
+	ioutil.WriteFile("echochat.json", k, 0644)
 }
 
 const (
